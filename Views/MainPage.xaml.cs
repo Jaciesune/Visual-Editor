0993d2eb71cb16b0b51401b258f7de80ad25ecbb
Jaciesune <lutasek9@gmail.com>
2025-10-27 14:48:12 +0100

    feat: Added RGB color choose option with view option of current color

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -13,6 +13,24 @@ namespace VE.Views
             BValue.TextChanged += OnRgbChanged;
         }
 
+        private bool _brushOpen = false;
+        private async void ShowBrushOptions(object sender, EventArgs e)
+        {
+            if (_brushOpen)
+            {
+                await BrushOptionsPanel.TranslateTo(-200, 0, 170, Easing.CubicIn); // chowamy w lewo
+                BrushOptionsPanel.IsVisible = false;
+                _brushOpen = false;
+            }
+            else
+            {
+                BrushOptionsPanel.TranslationX = -200;
+                BrushOptionsPanel.IsVisible = true;
+                await BrushOptionsPanel.TranslateTo(0, 0, 230, Easing.CubicOut); // wjeżdża z lewej
+                _brushOpen = true;
+            }
+        }
+
         private void OnRgbChanged(object sender, TextChangedEventArgs e)
         {
             if (int.TryParse(RValue.Text, out int r) &&
