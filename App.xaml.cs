b03b52f649b2ea3dc503d3abb3a769853dc0c003
Jaciesune <lutasek9@gmail.com>
2025-10-27 13:44:44 +0100

    init: Initialization and base program

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VE
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace VE
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
