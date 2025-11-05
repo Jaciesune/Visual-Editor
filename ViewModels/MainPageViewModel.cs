7fa8fc3c914a230d530330191692ef266f0bc5b5
Jaciesune <lutasek9@gmail.com>
2025-11-05 14:53:21 +0100

    feaf: adding files to canvas now is possible

--- ViewModels/MainPageViewModel.cs
+++ ViewModels/MainPageViewModel.cs
@@ -1,13 +1,24 @@
-﻿using System.ComponentModel;
+﻿using Microsoft.Maui.Graphics;
+using System.ComponentModel;
+using System.IO;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
 using VE.Models;
-using Microsoft.Maui.Graphics;
 
 namespace VE.ViewModels
 {
     public class MainPageViewModel : INotifyPropertyChanged
     {
         public BrushSettings Brush { get; set; } = new BrushSettings();
+        
+        private ImageSource _canvasImage;
+        public ImageSource CanvasImage
+        {
+            get => _canvasImage;
+            set { _canvasImage = value; OnPropertyChanged(nameof(CanvasImage)); }
+        }
+
+        public ICommand OpenImageCommand { get; }
 
         public MainPageViewModel()
         {
@@ -15,6 +26,43 @@ namespace VE.ViewModels
             {
                 OnPropertyChanged(nameof(BrushColor));
             };
+
+            OpenImageCommand = new Command(async () => await OpenImageAsync());
+        }
+
+        private async Task OpenImageAsync()
+        {
+            ImageLoadError = null;
+            try
+            {
+                var result = await FilePicker.PickAsync(new PickOptions
+                {
+                    PickerTitle = "Wybierz obraz...",
+                    FileTypes = FilePickerFileType.Images
+                });
+
+                if (result == null)
+                    return;
+
+                var ext = Path.GetExtension(result.FileName).ToLowerInvariant();
+                if (ext != ".jpg" && ext != ".jpeg" && ext != ".png")
+                {
+                    ImageLoadError = "Obsługiwane są wyłącznie pliki PNG i JPG.";
+                    return;
+                }
+
+                using var fileStream = await result.OpenReadAsync();
+
+                using var ms = new MemoryStream();
+                await fileStream.CopyToAsync(ms);
+                ms.Position = 0;
+
+                CanvasImage = ImageSource.FromStream(() => new MemoryStream(ms.ToArray()));
+
+            }
+            catch (Exception ex) {
+                ImageLoadError = "Błąd odczytu pliku: " + ex.Message;
+            }
         }
 
         public Color BrushColor => Color.FromRgb(Brush.R, Brush.G, Brush.B);
@@ -22,5 +70,13 @@ namespace VE.ViewModels
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged(string propertyName)
             => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        private string _imageLoadError;
+        public string ImageLoadError
+        {
+            get => _imageLoadError;
+            set { _imageLoadError = value; OnPropertyChanged(nameof(ImageLoadError)); }
+        }
+
     }
 }
