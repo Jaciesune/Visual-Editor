680d61681565e435f5dc66e842b1445f31783db3
Jaciesune <lutasek9@gmail.com>
2025-11-05 16:09:54 +0100

    chore: added setting def val for rgb to 0 after erasing all values

---
+++ Tools/RGBStringToIntConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace VE.Tools
+{
+    public class RgbStringToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value?.ToString() ?? "0";
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (int.TryParse(value as string, out int result))
+                return Math.Clamp(result, 0, 255);
+            return 0;
+        }
+    }
+}
